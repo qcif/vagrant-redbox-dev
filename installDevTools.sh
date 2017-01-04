996cea26f6fd32dee1fdfa3877e3d561883b1410
Andrew Brazzatti <a.brazzatti@qcif.edu.au>
2017-01-04 15:02:05 +1030

    Fixed usermod call so that vagrant user is allowed to run docker commands

--- installDevTools.sh
+++ installDevTools.sh
@@ -13,7 +13,7 @@ fi
 
 if [ ! -f "/usr/bin/docker-compose" ]; then
 wget -qO- https://get.docker.com/ | sh
-sudo usermod -aG docker $(whoami)
+sudo usermod -aG docker vagrant
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 sudo yum install -y epel-release
