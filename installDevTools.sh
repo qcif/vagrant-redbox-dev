fe127a68d5799d0b632337e7536ff0c0ec31dded
mattRedBox <matt@redboxresearchdata.com.au>
2020-09-10 12:05:07 +1000

    Updated setup after install for uow.

--- installDevTools.sh
+++ installDevTools.sh
@@ -38,11 +38,12 @@ if [ ! -f "/usr/bin/unison" ]; then
   cp src/unison /bin/
 fi
 
-sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install 8.9.4
+nvm install 12 --lts
+npm install -g @angular/cli
 
 # installing ntpd
 sudo yum install -y ntp
